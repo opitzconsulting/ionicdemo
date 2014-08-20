ca6dc28eadfe810213fcbb918828e65df1ab7063
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2014-08-20 15:15:28 +0200

    todolist: Show simple tasklist on dashboard with local storage backend

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,21 @@
-angular.module('starter.controllers', [])
+(function (angular) {
+    'use strict';
 
-.controller('DashCtrl', function($scope) {
-})
+    var controllers = angular.module('starter.controllers', ['starter.services']);
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
+    controllers.controller('DashCtrl', function ($scope, Tasks) {
+        $scope.tasks = Tasks.all();
+    });
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+    controllers.controller('FriendsCtrl', function ($scope, Friends) {
+        $scope.friends = Friends.all();
+    });
 
-.controller('AccountCtrl', function($scope) {
-});
+    controllers.controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
+        $scope.friend = Friends.get($stateParams.friendId);
+    });
+
+    controllers.controller('AccountCtrl', function ($scope) {
+    });
+
+})(window.angular);
--- www/js/services.js
+++ www/js/services.js
@@ -1,26 +1,60 @@
-angular.module('starter.services', [])
+(function (angular) {
+    'use strict';
 
-/**
- * A simple example service that returns some data.
- */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
+    var services = angular.module('starter.services', []);
 
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
+    services.factory('Tasks', function ($localStorage) {
+        var storage = $localStorage.$default({tasks: [], initalized: false});
 
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
-    }
-  }
-});
+        function add(task) {
+            storage.tasks.push(task);
+        }
+
+        function all() {
+            return storage.tasks;
+        }
+
+        function init() {
+            if (!storage.initalized) {
+                add({ title: 'Install ionic' });
+                add({ title: 'Create Todo app' });
+                add({ title: 'Make money!' });
+                storage.initalized = true;
+            }
+        }
+
+        return {
+            add: add,
+            all: all,
+            init: init
+        }
+
+    });
+
+    services.factory('Friends', function () {
+        // Might use a resource here that returns a JSON array
+
+        // Some fake testing data
+        var friends = [
+            { id: 0, name: 'Scruff McGruff' },
+            { id: 1, name: 'G.I. Joe' },
+            { id: 2, name: 'Miss Frizzle' },
+            { id: 3, name: 'Ash Ketchum' }
+        ];
+
+        return {
+            all: function () {
+                return friends;
+            },
+            get: function (friendId) {
+                // Simple index lookup
+                return friends[friendId];
+            }
+        }
+    });
+
+    services.run(function (Tasks) {
+        Tasks.init();
+    });
+
+})(window.angular);
