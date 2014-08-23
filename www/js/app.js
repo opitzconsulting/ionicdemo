187fea12a0125e6c1376325b5da34ca53d26a000
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2014-08-23 19:58:43 +0200

    qrcode-scanning-cleanup: cleanup

--- www/js/app.js
+++ www/js/app.js
@@ -26,7 +26,6 @@
 
     app.run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
-            // Hide the accessory bar by default
             if (window.cordova && window.cordova.plugins.Keyboard) {
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             }
